8856ba9f1bed989f4d55fe883e56ce265bf449c6
Jeffrey Martin <jemartin@nvidia.com>
2024-04-23 13:55:21 -0500

    force description read as utf-8
    Signed-off-by: Jeffrey Martin <jemartin@nvidia.com>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='ecoji',
     version=__version__,
     description='Encode and decode data as emojis.',
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     author='mecforlove',
     author_email='mecforlove@outlook.com',
     url='https://github.com/mecforlove/ecoji-py',
